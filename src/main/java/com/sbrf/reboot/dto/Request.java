9773fcf98c3326a26d0b7d1f6cd03ecd7afbd9a3
Xed1000000 <Git31415%>
2021-12-02 18:23:07 +0300

    homework - Lesson9

---
+++ src/main/java/com/sbrf/reboot/dto/Request.java
@@ -0,0 +1,13 @@
+package com.sbrf.reboot.dto;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+public class Request {
+    private String atmNumber;
+}
